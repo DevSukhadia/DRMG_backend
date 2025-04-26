0023959e194a6dec7e719f3dfc4c67f271d81399
DevSukhadia <devsukhadia2017@gmail.com>
2025-04-26 19:47:29 -0300

    Enabled decryption again

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,8 +9,8 @@ exports.login = async (req, res) => {
   if (users.length === 0) return res.status(404).json({ message: "User not found" });
 
   const user = users[0];
-  // const match = await bcrypt.compare(password, user.password);
-  // if (!match) return res.status(401).json({ message: "Incorrect password" });
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(401).json({ message: "Incorrect password" });
 
   const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
     expiresIn: "1h",
