39a69e1cb52d6cd398e1257adf5a377767471859
DevSukhadia <devsukhadia2017@gmail.com>
2025-05-02 19:21:02 -0300

    Added API for regions

---
+++ routes/regions.js
@@ -0,0 +1,9 @@
+const router = require("./order.routes");
+
+router.get('/regions-ms', (req, res) => {
+    db.query('SELECT * FROM REGIONS_MS WHERE ISACTIVE = 1', (err, results) => {
+      if (err) return res.status(500).json({ error: err });
+      res.json(results);
+    });
+  });
+  
