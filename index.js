528f1a3ba10b626cf8aa6e46c4c064c6a5c6f489
DevSukhadia <devsukhadia2017@gmail.com>
2025-05-02 19:38:29 -0300

    Updated CORS

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ const userRoutes = require("./routes/user.routes");
 const orderRoutes = require("./routes/order.routes");      
 const customerRoutes = require("./routes/customer.routes");
 const provinceRoutes = require("./routes/province.routes");
+const regionRoutes = require("./routes/regions.routes");
 
 dotenv.config();
 const app = express();
-app.use(cors());
+const cors = require("cors");
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://orders.drmgmaritimes.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 
 app.use("/api", authRoutes);
@@ -19,6 +27,7 @@ app.use("/api", userRoutes);
 app.use("/api", orderRoutes);      
 app.use("/api", customerRoutes);   
 app.use("/api", provinceRoutes);
+app.use("/api", regionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
